834632d8fe01366577907a31e7baa6fab24976e7
monytitya <maotityamony23@mekong.edu.kh>
2026-07-13 00:12:09 +0700

    Update datatyps

---
+++ src/DataTypes/BDatatypes.kt
@@ -0,0 +1,17 @@
+package DataTypes
+
+fun main(){
+
+    //Int TO long
+        val intValue: Int = 100
+        val longValue: Long = intValue.toLong()
+        val doubleValue: Double = intValue.toDouble()
+        val floatValue: Float = intValue.toFloat()
+        val stringValue: String = intValue.toString()
+    //Output
+        println(longValue)
+        println(doubleValue)
+        println(floatValue)
+        println(stringValue)
+
+}
---
+++ src/DataTypes/CStringToNum.kt
@@ -0,0 +1,14 @@
+package DataTypes
+
+fun main(){
+
+    val strNumber = "2029"
+    val parsedInt = strNumber.toInt()        // String → Int
+    val parsedDouble = strNumber.toDouble() // String → Double
+    val parsedFloat = strNumber.toFloat()
+
+    println(parsedInt)
+    println(parsedDouble)
+    println(parsedFloat)
+
+}
